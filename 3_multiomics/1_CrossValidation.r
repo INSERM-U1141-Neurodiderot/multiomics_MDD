0d986455677703d3647d57dba9f80df67755bbcc
clmin <clmin@laposte.net>
2024-01-16 10:55:48 +0100

    Add files via upload

---
+++ 3_multiomics/1_CrossValidation.r
@@ -0,0 +1,47 @@
+library(dplyr)
+library(stringr)
+
+### input: covariables
+cov_pooled = readRDS(  file = "data/cov_pooled.RDS")
+
+### create 5 split of 5-fold
+cov_female = cov_pooled %>% dplyr::filter (SEX == "F" )
+cov_male = cov_pooled %>% dplyr::filter (SEX == "M" )
+
+cv_fold_female =list ()
+for (folds in c(1:5) ) {
+    cv = cov_female %>%
+          group_by(GROUP, SEX) %>% ### generates folds identicial in proportion in MDD vs Control and Male vs Female to the originanl dataset 
+          sample_frac(1) %>%
+          mutate(fold=rep(1:5, length.out=n())) %>%
+          ungroup
+    for (d in 1:5)
+        {cv_fold_female = append (cv_fold_female , list(list(train = cv %>% 
+                                                   filter (fold != d) %>% 
+                                                   .$Samp  , 
+                                               test = cv %>% 
+                                                   filter (fold == d) 
+                                                   %>% .$Samp ) ) ) }
+    }
+
+
+cv_fold_male =list ()
+for (folds in c(1:5) ) {
+    cv = cov_male %>%
+          group_by(GROUP, SEX) %>% ### generates folds identicial in proportion in MDD vs Control and Male vs male to the originanl dataset 
+          sample_frac(1) %>%
+          mutate(fold=rep(1:5, length.out=n())) %>%
+          ungroup
+    for (d in 1:5)
+        {cv_fold_male = append (cv_fold_male , list(list(train = cv %>% 
+                                                   filter (fold != d) %>% 
+                                                   .$Samp  , 
+                                               test = cv %>% 
+                                                   filter (fold == d) 
+                                                   %>% .$Samp ) ) ) }
+    }
+
+saveRDS(cv_fold_female , file = "cv_fold_female.RDS") 
+saveRDS(cv_fold_male , file = "cv_fold_male.RDS") 
+
+
